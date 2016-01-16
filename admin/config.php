531ea0dae7b2319fdcab54cf61400c0893cba796
vtthuan <vtthuan89@gmail.com>
2016-01-16 22:18:03 +0100

    commit upload
    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ppcosmetics.net/admin/');
+define('HTTP_CATALOG', 'http://ppcosmetics.net/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ppcosmetics.net/admin/');
+define('HTTPS_CATALOG', 'http://ppcosmetics.net/');
+
+// DIR
+define('DIR_APPLICATION', '/home/floorbrain/public_html/admin/');
+define('DIR_SYSTEM', '/home/floorbrain/public_html/system/');
+define('DIR_LANGUAGE', '/home/floorbrain/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/floorbrain/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/floorbrain/public_html/system/config/');
+define('DIR_IMAGE', '/home/floorbrain/public_html/image/');
+define('DIR_CACHE', '/home/floorbrain/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/floorbrain/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/floorbrain/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/floorbrain/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/floorbrain/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/floorbrain/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ppcosmetics');
+define('DB_PASSWORD', 'EtCaTlihNgx]');
+define('DB_DATABASE', 'ppcosmetics');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ppcosmetics.net/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ppcosmetics.net/');
+
+// DIR
+define('DIR_APPLICATION', '/home/floorbrain/public_html/catalog/');
+define('DIR_SYSTEM', '/home/floorbrain/public_html/system/');
+define('DIR_LANGUAGE', '/home/floorbrain/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/floorbrain/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/floorbrain/public_html/system/config/');
+define('DIR_IMAGE', '/home/floorbrain/public_html/image/');
+define('DIR_CACHE', '/home/floorbrain/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/floorbrain/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/floorbrain/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/floorbrain/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/floorbrain/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ppcosmetics');
+define('DB_PASSWORD', 'EtCaTlihNgx]');
+define('DB_DATABASE', 'ppcosmetics');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
